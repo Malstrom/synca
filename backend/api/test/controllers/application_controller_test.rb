a96e3e1df7727847d2b44a6330c15651f4540e47
Igor Mironenko <igormir87@gmail.com>
2026-05-27 02:14:40 +0300

    test(controllers): fix auth tests and add application_controller coverage

---
+++ backend/api/test/controllers/application_controller_test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ProtectedDummyController < ApplicationController
+  def index
+    render json: { user_id: current_user.id }
+  end
+end
+
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    Rails.application.routes.draw do
+      get "/dummy_protected", to: "protected_dummy#index"
+    end
+  end
+
+  teardown do
+    Rails.application.reload_routes!
+  end
+
+  test "request without token returns 401" do
+    get "/dummy_protected"
+    assert_response :unauthorized
+  end
+
+  test "request with invalid token returns 401" do
+    get "/dummy_protected", headers: { "Authorization" => "Bearer invalid.token.here" }
+    assert_response :unauthorized
+  end
+
+  test "request with valid token returns 200 and current_user" do
+    user = users(:bob)
+    token = JwtService.access_token(user)
+    get "/dummy_protected", headers: { "Authorization" => "Bearer #{token}" }
+    assert_response :ok
+    assert_equal user.id, response.parsed_body["user_id"]
+  end
+end
