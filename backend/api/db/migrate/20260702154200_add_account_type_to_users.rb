c7544c7e7ad7f72a75fee33cea768eae17244064
Igor Mironenko <igormir87@gmail.com>
2026-07-02 20:09:01 +0400

    Modify migration for account_type column in users
    Updated migration to use ActiveRecord version 8.0 and added an index for account_type.

--- backend/api/db/migrate/20260702154200_add_account_type_to_users.rb
+++ backend/api/db/migrate/20260702154200_add_account_type_to_users.rb
@@ -1,5 +1,7 @@
-class AddAccountTypeToUsers < ActiveRecord::Migration[7.1]
+class AddAccountTypeToUsers < ActiveRecord::Migration[8.0]
   def change
     add_column :users, :account_type, :integer, default: 0, null: false
+
+    add_index :users, :account_type  
   end
 end
