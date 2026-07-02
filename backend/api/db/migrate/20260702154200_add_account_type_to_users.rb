85e033603c774853a704e6381c6e3602ef940947
Igor Mironenko <igormir87@gmail.com>
2026-07-02 20:13:20 +0400

    Delete backend/api/db/migrate/20260702154200_add_account_type_to_users.rb

--- backend/api/db/migrate/20260702154200_add_account_type_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddAccountTypeToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users, :account_type, :integer, default: 0, null: false
-
-    add_index :users, :account_type  
-  end
-end
