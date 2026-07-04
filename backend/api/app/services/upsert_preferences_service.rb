d3de0a351c9f1f2ece4b660bbb8eb9cbc8c97992
Igor Mironenko <igormir87@gmail.com>
2026-07-04 04:37:37 +0300

    feat: implement issue #100 — app/services/upsert_preferences_service.rb

---
+++ backend/api/app/services/upsert_preferences_service.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class UpsertPreferencesService
+  include Dry::Monads[:result]
+
+  def self.call(...) = new.call(...)
+
+  def call(current_user:, attrs:)
+    preference_profile = current_user.preference_profile || current_user.build_preference_profile
+
+    preference_profile.assign_attributes(attrs)
+    if preference_profile.save
+      Success(preference_profile)
+    else
+      Failure([ :validation_failed, preference_profile.errors.full_messages.first ])
+    end
+  end
+end
