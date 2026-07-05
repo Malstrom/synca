cc8eedf3795cc2e897d1426d855c1be0afd0b537
Igor Mironenko <igormir87@gmail.com>
2026-07-05 22:48:18 +0300

    chore: rubocop autocorrect

--- backend/api/test/controllers/api/v1/signals/preferences_controller_test.rb
+++ backend/api/test/controllers/api/v1/signals/preferences_controller_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::Signals::PreferencesControllerTest < ApiTestCase
   setup do
@@ -8,20 +8,20 @@ class Api::V1::Signals::PreferencesControllerTest < ApiTestCase
     @headers = auth_headers(@user)
   end
 
-  test "successful update" do
+  test 'successful update' do
     post api_v1_signals_preferences_path, params: {
       preferences: {
         sleep_together_importance: 3,
-        temperature_preference: "cool"
+        temperature_preference: 'cool'
       }
     }, headers: @headers
 
     assert_response :success
-    assert_equal 3, response.parsed_body["preferences"]["sleep_together_importance"]
-    assert_equal "cool", response.parsed_body["preferences"]["temperature_preference"]
+    assert_equal 3, response.parsed_body['preferences']['sleep_together_importance']
+    assert_equal 'cool', response.parsed_body['preferences']['temperature_preference']
   end
 
-  test "partial update" do
+  test 'partial update' do
     post api_v1_signals_preferences_path, params: {
       preferences: {
         rhythm_importance: 4
@@ -29,11 +29,11 @@ class Api::V1::Signals::PreferencesControllerTest < ApiTestCase
     }, headers: @headers
 
     assert_response :success
-    assert_equal 4, response.parsed_body["preferences"]["rhythm_importance"]
-    assert_nil response.parsed_body["preferences"]["sleep_together_importance"]
+    assert_equal 4, response.parsed_body['preferences']['rhythm_importance']
+    assert_nil response.parsed_body['preferences']['sleep_together_importance']
   end
 
-  test "validation failure" do
+  test 'validation failure' do
     post api_v1_signals_preferences_path, params: {
       preferences: {
         sleep_together_importance: 6
@@ -41,10 +41,10 @@ class Api::V1::Signals::PreferencesControllerTest < ApiTestCase
     }, headers: @headers
 
     assert_response :unprocessable_entity
-    assert_equal "validation_failed", response.parsed_body["error"]["code"]
+    assert_equal 'validation_failed', response.parsed_body['error']['code']
   end
 
-  test "unauthorized" do
+  test 'unauthorized' do
     post api_v1_signals_preferences_path, params: {
       preferences: {
         sleep_together_importance: 3
