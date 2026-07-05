028158495ba795cdd18002885440dce727ad54b4
Igor Mironenko <igormir87@gmail.com>
2026-07-05 23:56:14 +0300

    chore: rubocop autocorrect

--- backend/api/test/controllers/api/v1/signals/preferences_controller_test.rb
+++ backend/api/test/controllers/api/v1/signals/preferences_controller_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::Signals::PreferencesControllerTest < ApiTestCase
   setup do
@@ -8,7 +8,7 @@ class Api::V1::Signals::PreferencesControllerTest < ApiTestCase
     @headers = auth_headers(@user)
   end
 
-  test "successful update" do
+  test 'successful update' do
     post api_v1_signals_preferences_path,
          params: { preferences: { sleep_together_importance: 4 } },
          headers: @headers,
@@ -18,44 +18,44 @@ class Api::V1::Signals::PreferencesControllerTest < ApiTestCase
     assert_equal 4, json_response[:preferences][:sleep_together_importance]
   end
 
-  test "partial update" do
+  test 'partial update' do
     post api_v1_signals_preferences_path,
-         params: { preferences: { temperature_preference: "cool" } },
+         params: { preferences: { temperature_preference: 'cool' } },
          headers: @headers,
          as: :json
 
     assert_response :success
-    assert_equal "cool", json_response[:preferences][:temperature_preference]
+    assert_equal 'cool', json_response[:preferences][:temperature_preference]
   end
 
-  test "validation failure" do
+  test 'validation failure' do
     post api_v1_signals_preferences_path,
          params: { preferences: { rhythm_importance: 6 } },
          headers: @headers,
          as: :json
 
     assert_response :unprocessable_entity
-    assert_equal "validation_failed", json_response[:error][:code]
+    assert_equal 'validation_failed', json_response[:error][:code]
   end
 
-  test "unauthorized" do
+  test 'unauthorized' do
     post api_v1_signals_preferences_path,
-         params: { preferences: { movement_preference: "moderate" } },
+         params: { preferences: { movement_preference: 'moderate' } },
          as: :json
 
     assert_response :unauthorized
   end
 
-  test "guest user can update" do
+  test 'guest user can update' do
     guest_user = users(:guest_user)
     headers = auth_headers(guest_user)
 
     post api_v1_signals_preferences_path,
-         params: { preferences: { self_chronotype: "night" } },
+         params: { preferences: { self_chronotype: 'night' } },
          headers: headers,
          as: :json
 
     assert_response :success
-    assert_equal "night", json_response[:preferences][:self_chronotype]
+    assert_equal 'night', json_response[:preferences][:self_chronotype]
   end
 end
--- backend/api/test/models/preference_profile_test.rb
+++ backend/api/test/models/preference_profile_test.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class PreferenceProfileTest < ActiveSupport::TestCase
-  test "enums are defined correctly" do
+  test 'enums are defined correctly' do
     assert_equal %w[cool warm no_preference], PreferenceProfile.temperature_preferences.keys
     assert_equal %w[very_little moderate a_lot as_much_as_possible], PreferenceProfile.movement_preferences.keys
     assert_equal %w[morning night depends], PreferenceProfile.self_chronotypes.keys
   end
 
-  test "validates sleep_together_importance range" do
+  test 'validates sleep_together_importance range' do
     profile = PreferenceProfile.new(sleep_together_importance: 6)
     assert_not profile.valid?
-    assert_includes profile.errors[:sleep_together_importance], "must be between 1 and 5"
+    assert_includes profile.errors[:sleep_together_importance], 'must be between 1 and 5'
 
     profile.sleep_together_importance = 3
     assert profile.valid?
   end
 
-  test "validates rhythm_importance range" do
+  test 'validates rhythm_importance range' do
     profile = PreferenceProfile.new(rhythm_importance: 0)
     assert_not profile.valid?
-    assert_includes profile.errors[:rhythm_importance], "must be between 1 and 5"
+    assert_includes profile.errors[:rhythm_importance], 'must be between 1 and 5'
 
     profile.rhythm_importance = 4
     assert profile.valid?
   end
 
-  test "allows nil values for all fields" do
+  test 'allows nil values for all fields' do
     profile = PreferenceProfile.new
     assert profile.valid?
   end
