acc9cd30a8ea3372f551615d577789ee606bd93d
Igor Mironenko <igormir87@gmail.com>
2026-07-06 16:30:20 +0300

    chore: rubocop autocorrect

--- backend/api/config/routes.rb
+++ backend/api/config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
       end
 
       namespace :signals do
-        post :preferences, to: 'preferences#create'
+        post :preferences, to: "preferences#create"
       end
     end
   end
