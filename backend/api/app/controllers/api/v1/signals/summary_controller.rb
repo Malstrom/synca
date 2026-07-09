8f0d21416798f6a7b979f631701706dfec15dab7
Igor Mironenko <igormir87@gmail.com>
2026-07-10 02:37:37 +0300

    fix: inherit SummaryController from ApplicationController (not Api::V1::BaseController)

--- backend/api/app/controllers/api/v1/signals/summary_controller.rb
+++ backend/api/app/controllers/api/v1/signals/summary_controller.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
-class Api::V1::Signals::SummaryController < Api::V1::BaseController
-  before_action :authenticate_user!
+module Api
+  module V1
+    module Signals
+      class SummaryController < ApplicationController
+        before_action :authenticate_user!
 
-  def show
-    case SignalsSummaryService.call(user: current_user)
-    in Success[record]
-      render_success(SignalsSummarySerializer.new(record).as_json)
-    in Failure[:no_signals, message]
-      render_error(:no_signals, message)
+        def show
+          case SignalsSummaryService.call(user: current_user)
+          in Success[record]
+            render_success(SignalsSummarySerializer.new(record).as_json)
+          in Failure[:no_signals, message]
+            render_error(:no_signals, message)
+          end
+        end
+      end
     end
   end
 end
