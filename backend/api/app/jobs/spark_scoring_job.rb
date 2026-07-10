305498ba33345b884ff9d2e81943aab331e5bd30
Igor Mironenko <igormir87@gmail.com>
2026-07-10 13:34:11 +0300

    chore: rubocop autocorrect

--- backend/api/app/jobs/spark_scoring_job.rb
+++ backend/api/app/jobs/spark_scoring_job.rb
@@ -7,7 +7,7 @@ class SparkScoringJob < ApplicationJob
     RewardEngine.call(spark)
 
     [ spark.initiator, spark.partner ].each do |user|
-      next unless user.account_type == 'guest'
+      next unless user.account_type == "guest"
       result = MagicLinkService.call(user: user)
       next if result.failure?
       GuestMailer.magic_link(user, result.value!).deliver_later
